240aec5a2ecc19f655d3b3d9a8c8a20dbcd97759
luoxiaofeng <954118124@qq.com>
2022-05-07 22:50:28 +0800

    基础目录调整

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -26,7 +26,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     nav: [
       { text: '首页', link: '/' },
       {
-        text: '知识整理',
+        text: '学习笔记',
         link: '/pages/8611ff/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
       },
       
@@ -219,26 +219,38 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         },
       },
     ],
-    // [
-    //   'vuepress-plugin-comment', // 评论
-    //   {
-    //     choosen: 'gitalk',
-    //     options: {
-    //       clientID: 'a6e1355287947096b88b',
-    //       clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-    //       repo: 'blog-gitalk-comment', // GitHub 仓库
-    //       owner: '954118124', // GitHub仓库所有者
-    //       admin: ['954118124'], // 对仓库有写权限的人
-    //       // distractionFreeMode: true,
-    //       pagerDirection: 'last', // 'first'正序 | 'last'倒序
-    //       id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-    //       title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-    //       labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-    //       body:
-    //         '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-    //     },
-    //   },
-    // ],
+    //动态彩条
+    [
+      "ribbon-animation", {
+      size: 90,   // 默认数据
+      opacity: 0.3,  //  透明度
+      zIndex: -1,   //  层级
+      opt: {
+        // 色带HSL饱和度
+        colorSaturation: "80%",
+        // 色带HSL亮度量
+        colorBrightness: "60%",
+        // 带状颜色不透明度
+        colorAlpha: 0.65,
+        // 在HSL颜色空间中循环显示颜色的速度有多快
+        colorCycleSpeed: 6,
+        // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+        verticalPosition: "center",
+        // 到达屏幕另一侧的速度有多快
+        horizontalSpeed: 200,
+        // 在任何给定时间，屏幕上会保留多少条带
+        ribbonCount: 5,
+        // 添加笔划以及色带填充颜色
+        strokeSize: 0,
+        // 通过页面滚动上的因子垂直移动色带
+        parallaxAmount: 0,
+        // 随着时间的推移，为每个功能区添加动画效果
+        animateSections: true
+      },
+      ribbonShow: false, //  点击彩带  true显示  false为不显示
+      ribbonAnimationShow: true  // 滑动彩带
+    }
+    ],
   ],
 
   markdown: {
